dfd939b8efd938ba327fe8de4f6caba094cd4a94
Grace Ho <rainczech@gmail.com>
2018-06-20 14:04:28 -0500

    yay something finally rendered

--- react-memory-game/src/App.js
+++ react-memory-game/src/App.js
@@ -6,9 +6,21 @@ import images from "./images.json";
 import Image from "./components/Image";
 
 class App extends Component {
+  componentDidMount(){
+    console.log("Images are ", images)
+  }
   render() {
     return (
-      {Image}
+      <div className="App">
+      {images.map(function(image, index){
+        return(
+          <div key = {image.id}><img src={image.url} alt={image.alt}/>
+          </div>
+        )
+      })
+    }
+      </div>
+      
       // {images.map(image => (
       //   <Image
       //   url={image.url}
@@ -21,20 +33,21 @@ class App extends Component {
 
 
 
+
   // boiler code
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <h1 className="App-title">Welcome to React</h1>
+  //       </header>
+  //       <p className="App-intro">
+  //         To get started, edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
