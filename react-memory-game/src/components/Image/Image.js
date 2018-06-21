8a53061a8df7df5d508bdd155d7d25f9a342b760
Grace Ho <rainczech@gmail.com>
2018-06-20 22:34:22 -0500

    clean-up image.js

--- react-memory-game/src/components/Image/Image.js
+++ react-memory-game/src/components/Image/Image.js
@@ -1,16 +1,3 @@
-// import React from "react";
-// import "./Image.css";
-
-// const Image = props => (
-//         <div className="col-md-2">
-//              <div className="card" key={props.id}>
-//                 <img src={props.url} alt={props.alt}/>
-//             </div>
-//         </div>
-// )
-
-// export default Image;
-
 import React, { Component } from "react";
 
 import './Image.css';
